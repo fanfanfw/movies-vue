475fa9f495805582bf57c5d8437826ceea294727
fanfanfw <firgiawan.fanfan@gmail.com>
2026-04-29 19:38:06 +0700

    update footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -6,27 +6,6 @@
         Nuxt Movies
       </div>
     </div>
-    <div flex="~ row gap2" items-center>
-      <div op50 text-sm>
-        Made with
-      </div>
-      <a href="https://nuxt.com/" target="_blank" title="Nuxt 3">
-        <IconNuxt3 h5 />
-      </a>
-    </div>
-    <div flex="~ row gap2" items-center>
-      <p text-true-gray:90 text-sm>
-        Data provided by&nbsp;
-        <a op100 target="_blank" href="https://www.themoviedb.org/" title="The Movie Database" rel="noopener" n-link-text>
-          <IconTMDB inline h4 />
-        </a>
-      </p>
-    </div>
-    <div flex="~ row gap2" items-center>
-      <p text-true-gray:90 text-sm>
-        This project uses the TMDB API but is not endorsed or certified by TMDB.
-      </p>
-    </div>
     <div flex gap4 items-center>
       <ExternalLinks
         :links="{
